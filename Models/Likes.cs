b8842293331cfd25ff9d4f729d28a1dafe3e0e8f
etudiant-no.202292399 <116734536+AlexaandreNgoma@users.noreply.github.com>
2026-04-10 00:11:45 -0400

    Add project files.

---
+++ Models/Likes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL;
+
+namespace Models
+{
+    public class Likes : Record
+    {
+        public int MediaId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Models/LikesRepository.cs
@@ -0,0 +1,29 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class LikesRepository : Repository<Likes>
+        {
+            public List<Likes> GetByMedia(int mediaId)
+                => ToList().Where(l => l.MediaId == mediaId).ToList();
+
+            public Likes GetUserLike(int mediaId, int userId)
+                => ToList().FirstOrDefault(l => l.MediaId == mediaId && l.UserId == userId);
+
+            public void DeleteByMedia(int mediaId)
+            {
+                foreach (var like in GetByMedia(mediaId))
+                    base.Delete(like.Id);
+            }
+
+            public void DeleteByUser(int userId)
+            {
+                foreach (var like in ToList().Where(l => l.UserId == userId).ToList())
+                    base.Delete(like.Id);
+            }
+        }
+}
